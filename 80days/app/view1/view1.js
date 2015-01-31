4968039a618b4f55e22e8d8a4697c64197c98dbd
John Gill <swfiua@gmail.com>
2015-01-31 16:57:18 -0400

    inject $http, not $html.

--- 80days/app/view1/view1.js
+++ 80days/app/view1/view1.js
@@ -1,19 +1,19 @@
 'use strict';
 
-angular.module('myApp.view1', ['ngRoute'])
+var app = angular.module('myApp.view1', ['ngRoute'])
 
-.config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/view1', {
-    templateUrl: 'view1/view1.html',
-    controller: 'View1Ctrl'
-  });
-}])
+    .config(['$routeProvider', function($routeProvider) {
+	$routeProvider.when('/view1', {
+	    templateUrl: 'view1/view1.html',
+	    controller: 'View1Ctrl'
+	});
+    }]);
 
-.controller('View1Ctrl', ['$html', function($html) {
+app.controller('View1Ctrl', [ '$http', function($http) {
     var view = this;
 
-    $html.get('/today.json').success(function(data){
-	view.today = data['today'];
-    });
+//    $http.get('/today.json').success(function(data){
+//	view.today = data['today'];
+//    });
     this.today = 1422633165.465848 * 1000;
 }]);
