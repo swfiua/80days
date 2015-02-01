8789ea7a6d051e4794d9864822b9226ae032c14a
John Gill <swfiua@gmail.com>
2015-02-01 15:11:17 -0400

    Fix controller to use django API to get date.

--- 80days/static/app/home/home.js
+++ 80days/static/app/home/home.js
@@ -12,8 +12,8 @@ var app = angular.module('myApp.home', ['ngRoute'])
 app.controller('HomeCtrl', [ '$http', function($http) {
     var view = this;
 
-//    $http.get('/today.json').success(function(data){
-//	view.today = data['today'];
-//    });
-    this.today = 1422633165.465848 * 1000;
+    $http.get('../../eighty/datetime_as_timestamp').success(function(data){
+	view.today = data['now'];
+    });
+    //this.today = 1422633165.465848 * 1000;
 }]);
