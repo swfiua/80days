3fc02060a9a9df23a7bfb35b82add9b2e9479e38
John Gill <swfiua@gmail.com>
2015-02-01 13:08:16 -0400

    Re-organise code, break everything ;)

---
+++ 80days/static/app/competitions/competitions.js
@@ -0,0 +1,14 @@
+'use strict';
+
+angular.module('myApp.competitions', ['ngRoute'])
+
+.config(['$routeProvider', function($routeProvider) {
+  $routeProvider.when('/competitions', {
+    templateUrl: 'competitions/competitions.html',
+    controller: 'CompetitionsCtrl'
+  });
+}])
+
+.controller('CompetitionsCtrl', [function() {
+
+}]);
