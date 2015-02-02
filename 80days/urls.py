f8b05b68dcaa69d9ed31b5dd43551bd077e519e3
John Gill <swfiua@gmail.com>
2015-02-02 12:24:00 -0400

    Start adding django quiz

--- 80days/urls.py
+++ 80days/urls.py
@@ -85,5 +85,8 @@ urlpatterns +=  patterns('',
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     url(r'^docs/', include('rest_framework_swagger.urls')),
 
+    # django-quiz
+    url(r'^quiz/', include('quiz.urls')),
+
 
 ) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
